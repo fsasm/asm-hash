f0c7107718ba1ace17ec51d88a7e1247e92323f6
Fabjan Sukalia <fsukalia@gmail.com>
2014-05-18 01:04:48 +0200

    faster md5 (assembler)

--- src/md5_block.s
+++ src/md5_block.s
@@ -77,269 +77,657 @@ md5_table:
 .text
 .global md5_process_blocks_asm
 md5_process_blocks_asm: /* (uint8_t block[64], uint32_t hash[4], uint n) */
-	push {r4 - r12, lr}
+	push {r4 - r11}
 	cmp r2, #0
 	beq .Lend
 	
-	mov lr, r2
+	mov r3, r2
 	ldr r2, addr_table	
 	
 .Lstart:
 	/* r4 = h0; r5 = h1; r6 = h2; r7 = h3 */
 	ldmia  r1, {r4, r5, r6, r7}
-	mov r3, #0 /* loop counter */
-	mov r12, #0 /* block index */
-
-loop0_start:
-	/* 1. iteration */
-	ldr r10, [r0, r3, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	/* first loop */
+	ldr r10, [r0, +#0]
+	ldr r11, [r2, +#0]
 	bic r9, r7, r5
 	and r8, r5, r6
 	orr r8, r8, r9
 	add r4, r4, r8
 	add r10, r10, r11
 	add r4, r4, r10
-	add r3, r3, #1
+	
+	ldr r10, [r0, +#4]
+	ldr r11, [r2, +#4]
 	add r4, r5, r4, ROR #25
-
-	/* 2. iteration */
-	ldr r10, [r0, r3, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
 	bic r9, r6, r4
 	and r8, r4, r5
 	orr r8, r8, r9
 	add r7, r7, r8
 	add r10, r10, r11
 	add r7, r7, r10
-	add r3, r3, #1
-	add r7, r4, r7, ROR #20
 
-	/* 3. iteration */
-	ldr r10, [r0, r3, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r0, +#8]
+	ldr r11, [r2, +#8]
+	add r7, r4, r7, ROR #20
 	bic r9, r5, r7
 	and r8, r7, r4
 	orr r8, r8, r9
 	add r6, r6, r8
 	add r10, r10, r11
 	add r6, r6, r10
-	add r3, r3, #1
-	add r6, r7, r6, ROR #15
 
-	/* 4. iteration */
-	ldr r10, [r0, r3, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r0, +#12]
+	ldr r11, [r2, +#12]
+	add r6, r7, r6, ROR #15
 	bic r9, r4, r6
 	and r8, r6, r7
 	orr r8, r8, r9
 	add r5, r5, r8
 	add r10, r10, r11
 	add r5, r5, r10
-	add r3, r3, #1
+	
+	ldr r10, [r0, +#16]
+	ldr r11, [r2, +#16]
 	add r5, r6, r5, ROR #10
+	bic r9, r7, r5
+	and r8, r5, r6
+	orr r8, r8, r9
+	add r4, r4, r8
+	add r10, r10, r11
+	add r4, r4, r10
 
-loop0_check:
-	cmp r3, #16
-	blt loop0_start
-	mov r12, #1
+	ldr r10, [r0, +#20]
+	ldr r11, [r2, +#20]
+	add r4, r5, r4, ROR #25
+	bic r9, r6, r4
+	and r8, r4, r5
+	orr r8, r8, r9
+	add r7, r7, r8
+	add r10, r10, r11
+	add r7, r7, r10
 
-loop1_start:
-	/* 1. iteration */
-	ldr r11, [r2, r3, LSL #2]
-	ldr r10, [r0, r12, LSL #2]
+	ldr r10, [r0, +#24]
+	ldr r11, [r2, +#24]
+	add r7, r4, r7, ROR #20
+	bic r9, r5, r7
+	and r8, r7, r4
+	orr r8, r8, r9
+	add r6, r6, r8
+	add r10, r10, r11
+	add r6, r6, r10
+
+	ldr r10, [r0, +#28]
+	ldr r11, [r2, +#28]
+	add r6, r7, r6, ROR #15
+	bic r9, r4, r6
+	and r8, r6, r7
+	orr r8, r8, r9
+	add r5, r5, r8
+	add r10, r10, r11
+	add r5, r5, r10
+	
+	ldr r10, [r0, +#32]
+	ldr r11, [r2, +#32]
+	add r5, r6, r5, ROR #10
+	bic r9, r7, r5
+	and r8, r5, r6
+	orr r8, r8, r9
+	add r4, r4, r8
+	add r10, r10, r11
+	add r4, r4, r10
+
+	ldr r10, [r0, +#36]
+	ldr r11, [r2, +#36]
+	add r4, r5, r4, ROR #25
+	bic r9, r6, r4
+	and r8, r4, r5
+	orr r8, r8, r9
+	add r7, r7, r8
+	add r10, r10, r11
+	add r7, r7, r10
+
+	ldr r10, [r0, +#40]
+	ldr r11, [r2, +#40]
+	add r7, r4, r7, ROR #20
+	bic r9, r5, r7
+	and r8, r7, r4
+	orr r8, r8, r9
+	add r6, r6, r8
+	add r10, r10, r11
+	add r6, r6, r10
+
+	ldr r10, [r0, +#44]
+	ldr r11, [r2, +#44]
+	add r6, r7, r6, ROR #15
+	bic r9, r4, r6
+	and r8, r6, r7
+	orr r8, r8, r9
+	add r5, r5, r8
+	add r10, r10, r11
+	add r5, r5, r10
+	
+	ldr r10, [r0, +#48]
+	ldr r11, [r2, +#48]
+	add r5, r6, r5, ROR #10
+	bic r9, r7, r5
+	and r8, r5, r6
+	orr r8, r8, r9
+	add r4, r4, r8
+	add r10, r10, r11
+	add r4, r4, r10
+
+	ldr r10, [r0, +#52]
+	ldr r11, [r2, +#52]
+	add r4, r5, r4, ROR #25
+	bic r9, r6, r4
+	and r8, r4, r5
+	orr r8, r8, r9
+	add r7, r7, r8
+	add r10, r10, r11
+	add r7, r7, r10
+
+	ldr r10, [r0, +#56]
+	ldr r11, [r2, +#56]
+	add r7, r4, r7, ROR #20
+	bic r9, r5, r7
+	and r8, r7, r4
+	orr r8, r8, r9
+	add r6, r6, r8
+	add r10, r10, r11
+	add r6, r6, r10
+
+	ldr r10, [r0, +#60]
+	ldr r11, [r2, +#60]
+	add r6, r7, r6, ROR #15
+	bic r9, r4, r6
+	and r8, r6, r7
+	orr r8, r8, r9
+	add r5, r5, r8
+	add r10, r10, r11
+	add r5, r5, r10
+	
+	/* second loop */
+	ldr r11, [r2, +#64]
+	ldr r10, [r0, +#4]
+	add r5, r6, r5, ROR #10
 	bic r9, r6, r7
 	and r8, r7, r5
 	orr r9, r8, r9
-
-	add r3, r3, #1
 	add r10, r10, r11
 	add r4, r4, r9
-	add r12, r12, #5
 	add r4, r4, r10
-	and r12, r12, #0x0F
-	add r4, r5, r4, ROR #27 /* 5, 9, 14, 20 */
 
-	/* 2. iteration */
-	ldr r11, [r2, r3, LSL #2]
-	ldr r10, [r0, r12, LSL #2]
+	ldr r11, [r2, +#68]
+	ldr r10, [r0, +#24]
+	add r4, r5, r4, ROR #27
 	bic r9, r5, r6
 	and r8, r6, r4
 	orr r9, r8, r9
-
-	add r3, r3, #1
 	add r10, r10, r11
 	add r7, r7, r9
-	add r12, r12, #5
 	add r7, r7, r10
-	and r12, r12, #0x0F
-	add r7, r4, r7, ROR #23
 
-	/* 3. iteration */
-	ldr r11, [r2, r3, LSL #2]
-	ldr r10, [r0, r12, LSL #2]
+	ldr r11, [r2, +#72]
+	ldr r10, [r0, +#44]
+	add r7, r4, r7, ROR #23
 	bic r9, r4, r5
 	and r8, r5, r7
 	orr r9, r8, r9
-
-	add r3, r3, #1
 	add r10, r10, r11
 	add r6, r6, r9
-	add r12, r12, #5
 	add r6, r6, r10
-	and r12, r12, #0x0F
-	add r6, r7, r6, ROR #18
 
-	/* 4. iteration */
-	ldr r10, [r0, r12, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r2, +#76]
+	ldr r11, [r0, +#0]
+	add r6, r7, r6, ROR #18
 	bic r9, r7, r4
 	and r8, r4, r6
 	orr r9, r8, r9
-
-	add r3, r3, #1
 	add r10, r10, r11
 	add r5, r5, r9
 	add r5, r5, r10
-	add r12, r12, #5
+	
+	ldr r11, [r2, +#80]
+	ldr r10, [r0, +#20]
 	add r5, r6, r5, ROR #12
-loop1_check:
-	cmp r3, #32
-	blt loop1_start
-	mov r12, #5
+	bic r9, r6, r7
+	and r8, r7, r5
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
 
-loop2_start:
-	/* 1. iteration */
-	ldr r11, [r2, r3, LSL #2]
-	ldr r10, [r0, r12, LSL #2]
+	ldr r11, [r2, +#84]
+	ldr r10, [r0, +#40]
+	add r4, r5, r4, ROR #27
+	bic r9, r5, r6
+	and r8, r6, r4
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
+
+	ldr r11, [r2, +#88]
+	ldr r10, [r0, +#60]
+	add r7, r4, r7, ROR #23
+	bic r9, r4, r5
+	and r8, r5, r7
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#92]
+	ldr r11, [r0, +#16]
+	add r6, r7, r6, ROR #18
+	bic r9, r7, r4
+	and r8, r4, r6
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+	
+	ldr r11, [r2, +#96]
+	ldr r10, [r0, +#36]
+	add r5, r6, r5, ROR #12
+	bic r9, r6, r7
+	and r8, r7, r5
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
+
+	ldr r11, [r2, +#100]
+	ldr r10, [r0, +#56]
+	add r4, r5, r4, ROR #27
+	bic r9, r5, r6
+	and r8, r6, r4
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
+
+	ldr r11, [r2, +#104]
+	ldr r10, [r0, +#12]
+	add r7, r4, r7, ROR #23
+	bic r9, r4, r5
+	and r8, r5, r7
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#108]
+	ldr r11, [r0, +#32]
+	add r6, r7, r6, ROR #18
+	bic r9, r7, r4
+	and r8, r4, r6
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+	
+	ldr r11, [r2, +#112]
+	ldr r10, [r0, +#52]
+	add r5, r6, r5, ROR #12
+	bic r9, r6, r7
+	and r8, r7, r5
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
+
+	ldr r11, [r2, +#116]
+	ldr r10, [r0, +#8]
+	add r4, r5, r4, ROR #27
+	bic r9, r5, r6
+	and r8, r6, r4
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
+
+	ldr r11, [r2, +#120]
+	ldr r10, [r0, +#28]
+	add r7, r4, r7, ROR #23
+	bic r9, r4, r5
+	and r8, r5, r7
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#124]
+	ldr r11, [r0, +#48]
+	add r6, r7, r6, ROR #18
+	bic r9, r7, r4
+	and r8, r4, r6
+	orr r9, r8, r9
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+
+	/* third loop */
+	ldr r11, [r2, +#128]
+	ldr r10, [r0, +#20]
+	add r5, r6, r5, ROR #12
 	eor r8, r5, r6
-	add r12, r12, #3
 	eor r9, r8, r7
 	add r10, r10, r11
 	add r4, r4, r9
-	and r12, r12, #0x0F
 	add r4, r4, r10
-	add r3, r3, #1
-	add r4, r5, r4, ROR #28 /* 4, 11, 16, 23 */
 
-	/* 2. iteration */
-	ldr r10, [r0, r12, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r2, +#132]
+	ldr r11, [r0, +#32]
+	add r4, r5, r4, ROR #28
 	eor r8, r4, r5
-	add r12, r12, #3
 	eor r9, r8, r6
 	add r10, r10, r11
 	add r7, r7, r9
 	add r7, r7, r10
-	add r3, r3, #1
-	add r7, r4, r7, ROR #21
 
-	/* 3. iteration */
-	ldr r10, [r0, r12, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r2, +#136]
+	ldr r11, [r0, +#44]
+	add r7, r4, r7, ROR #21
 	eor r8, r7, r4
-	add r12, r12, #3
 	eor r9, r8, r5
 	add r10, r10, r11
 	add r6, r6, r9
-	and r12, r12, #0x0F
 	add r6, r6, r10
-	add r3, r3, #1
-	add r6, r7, r6, ROR #16
 
-	/* 4. iteration */
-	ldr r10, [r0, r12, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r2, +#140]
+	ldr r11, [r0, +#56]
+	add r6, r7, r6, ROR #16
 	eor r8, r6, r7
-	add r12, r12, #3
 	eor r9, r8, r4
 	add r10, r10, r11
 	add r5, r5, r9
-	and r12, r12, #0x0F
 	add r5, r5, r10
-	add r3, r3, #1
+
+	ldr r11, [r2, +#144]
+	ldr r10, [r0, +#4]
 	add r5, r6, r5, ROR #9
+	eor r8, r5, r6
+	eor r9, r8, r7
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
 
-loop2_check:
-	cmp r3, #48
-	blt loop2_start
-	mov r12, #0
+	ldr r10, [r2, +#148]
+	ldr r11, [r0, +#16]
+	add r4, r5, r4, ROR #28
+	eor r8, r4, r5
+	eor r9, r8, r6
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
 
-loop3_start:
-	/* 1. iteration */
-	ldr r11, [r2, r3, LSL #2]
-	ldr r10, [r0, r12, LSL #2]
+	ldr r10, [r2, +#152]
+	ldr r11, [r0, +#28]
+	add r7, r4, r7, ROR #21
+	eor r8, r7, r4
+	eor r9, r8, r5
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#156]
+	ldr r11, [r0, +#40]
+	add r6, r7, r6, ROR #16
+	eor r8, r6, r7
+	eor r9, r8, r4
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+	
+	ldr r11, [r2, +#160]
+	ldr r10, [r0, +#52]
+	add r5, r6, r5, ROR #9
+	eor r8, r5, r6
+	eor r9, r8, r7
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
+
+	ldr r10, [r2, +#164]
+	ldr r11, [r0, +#0]
+	add r4, r5, r4, ROR #28
+	eor r8, r4, r5
+	eor r9, r8, r6
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
+
+	ldr r10, [r2, +#168]
+	ldr r11, [r0, +#12]
+	add r7, r4, r7, ROR #21
+	eor r8, r7, r4
+	eor r9, r8, r5
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#172]
+	ldr r11, [r0, +#24]
+	add r6, r7, r6, ROR #16
+	eor r8, r6, r7
+	eor r9, r8, r4
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+
+	ldr r11, [r2, +#176]
+	ldr r10, [r0, +#36]
+	add r5, r6, r5, ROR #9
+	eor r8, r5, r6
+	eor r9, r8, r7
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
+
+	ldr r10, [r2, +#180]
+	ldr r11, [r0, +#48]
+	add r4, r5, r4, ROR #28
+	eor r8, r4, r5
+	eor r9, r8, r6
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
+
+	ldr r10, [r2, +#184]
+	ldr r11, [r0, +#60]
+	add r7, r4, r7, ROR #21
+	eor r8, r7, r4
+	eor r9, r8, r5
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#188]
+	ldr r11, [r0, +#8]
+	add r6, r7, r6, ROR #16
+	eor r8, r6, r7
+	eor r9, r8, r4
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+
+	/* fourth loop */
+	ldr r11, [r2, +#192]
+	ldr r10, [r0, +#0]
+	add r5, r6, r5, ROR #9
 	mvn r8, r7
-	add r12, r12, #7
 	orr r8, r5, r8
-	and r12, r12, #0x0F
 	eor r9, r8, r6
 	add r10, r10, r11
 	add r4, r4, r9
 	add r4, r4, r10
-	add r3, r3, #1
-	add r4, r5, r4, ROR #26 /* 6, 10, 15, 21 */
-
-
-	/* 2. iteration */
-	ldr r10, [r0, r12, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	
+	ldr r10, [r2, +#196]
+	ldr r11, [r0, +#28]
+	add r4, r5, r4, ROR #26 
 	mvn r8, r6
-	add r12, r12, #7
 	orr r8, r8, r4
-	and r12, r12, #0x0F
 	eor r9, r8, r5
 	add r10, r10, r11
 	add r7, r7, r9
 	add r7, r7, r10
-	add r3, r3, #1
-	add r7, r4, r7, ROR #22
 
-	/* 3. iteration */
-	ldr r10, [r0, r12, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r2, +#200]
+	ldr r11, [r0, +#56]
+	add r7, r4, r7, ROR #22
 	mvn r8, r5
-	add r12, r12, #7
 	orr r8, r8, r7
-	and r12, r12, #0x0F
 	eor r9, r8, r4
 	add r10, r10, r11
 	add r6, r6, r9
 	add r6, r6, r10
-	add r3, r3, #1
-	add r6, r7, r6, ROR #17
 
-	/* 4. iteration */
-	ldr r10, [r0, r12, LSL #2]
-	ldr r11, [r2, r3, LSL #2]
+	ldr r10, [r2, +#204]
+	ldr r11, [r0, +#20]
+	add r6, r7, r6, ROR #17
 	mvn r8, r4
-	add r12, r12, #7
 	orr r8, r8, r6
-	and r12, r12, #0x0F
 	eor r9, r8, r7
 	add r10, r10, r11
 	add r5, r5, r9
 	add r5, r5, r10
-	add r3, r3, #1
+	
+	ldr r11, [r2, +#208]
+	ldr r10, [r0, +#48]
 	add r5, r6, r5, ROR #11
+	mvn r8, r7
+	orr r8, r5, r8
+	eor r9, r8, r6
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
+	
+	ldr r10, [r2, +#212]
+	ldr r11, [r0, +#12]
+	add r4, r5, r4, ROR #26 
+	mvn r8, r6
+	orr r8, r8, r4
+	eor r9, r8, r5
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
 
-loop3_check:
-	cmp r3, #64
-	blt loop3_start
+	ldr r10, [r2, +#216]
+	ldr r11, [r0, +#40]
+	add r7, r4, r7, ROR #22
+	mvn r8, r5
+	orr r8, r8, r7
+	eor r9, r8, r4
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#220]
+	ldr r11, [r0, +#4]
+	add r6, r7, r6, ROR #17
+	mvn r8, r4
+	orr r8, r8, r6
+	eor r9, r8, r7
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+	
+	ldr r11, [r2, +#224]
+	ldr r10, [r0, +#32]
+	add r5, r6, r5, ROR #11
+	mvn r8, r7
+	orr r8, r5, r8
+	eor r9, r8, r6
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
+	
+	ldr r10, [r2, +#228]
+	ldr r11, [r0, +#60]
+	add r4, r5, r4, ROR #26 
+	mvn r8, r6
+	orr r8, r8, r4
+	eor r9, r8, r5
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
+
+	ldr r10, [r2, +#232]
+	ldr r11, [r0, +#24]
+	add r7, r4, r7, ROR #22
+	mvn r8, r5
+	orr r8, r8, r7
+	eor r9, r8, r4
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#236]
+	ldr r11, [r0, +#52]
+	add r6, r7, r6, ROR #17
+	mvn r8, r4
+	orr r8, r8, r6
+	eor r9, r8, r7
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
+	
+	ldr r11, [r2, +#240]
+	ldr r10, [r0, +#16]
+	add r5, r6, r5, ROR #11
+	mvn r8, r7
+	orr r8, r5, r8
+	eor r9, r8, r6
+	add r10, r10, r11
+	add r4, r4, r9
+	add r4, r4, r10
+	
+	ldr r10, [r2, +#244]
+	ldr r11, [r0, +#44]
+	add r4, r5, r4, ROR #26 
+	mvn r8, r6
+	orr r8, r8, r4
+	eor r9, r8, r5
+	add r10, r10, r11
+	add r7, r7, r9
+	add r7, r7, r10
+
+	ldr r10, [r2, +#248]
+	ldr r11, [r0, +#8]
+	add r7, r4, r7, ROR #22
+	mvn r8, r5
+	orr r8, r8, r7
+	eor r9, r8, r4
+	add r10, r10, r11
+	add r6, r6, r9
+	add r6, r6, r10
+
+	ldr r10, [r2, +#252]
+	ldr r11, [r0, +#36]
+	add r6, r7, r6, ROR #17
+	mvn r8, r4
+	orr r8, r8, r6
+	eor r9, r8, r7
+	add r10, r10, r11
+	add r5, r5, r9
+	add r5, r5, r10
 
 	ldmia r1, {r8, r9, r10, r11}
+	add r5, r6, r5, ROR #11
 	add r4, r4, r8
 	add r5, r5, r9
 	add r6, r6, r10
 	add r7, r7, r11
 	stmia r1, {r4, r5, r6, r7}
 	
-	subs lr, lr, #1
+	subs r3, r3, #1
 	addne r0, r0, #64
 	bne .Lstart
 .Lend:
 
-	pop {r4 - r12, lr}
+	pop {r4 - r11}
 	bx lr
 
 addr_table:
