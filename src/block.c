976e1b80aa9423aa73feb083371103fa8ad2370d
Fabjan Sukalia <fsukalia@gmail.com>
2014-09-30 01:01:43 +0200

    extending block with a new flag and an end function

--- src/block.c
+++ src/block.c
@@ -24,7 +24,23 @@ void block_init (block* b, block_size max_size, uint8_t* buffer, process_func fu
 	b->buffer = buffer;
 	b->max_size = max_size;
 	b->full_size = 0;
-	b->func = func;
+	b->pfunc = func;
+	b->efunc = NULL;
+	b->func_data = func_data;
+}
+
+void block_init_with_end (block* b, block_size max_size, uint8_t* buffer, process_func pfunc, end_func efunc, void* func_data) {
+	if (b == NULL || buffer == NULL || pfunc == NULL)
+		return;
+	
+	if (max_size != BLOCK_SIZE_512 && max_size != BLOCK_SIZE_1024)
+		return;
+	
+	b->buffer = buffer;
+	b->max_size = max_size;
+	b->full_size = 0;
+	b->pfunc = pfunc;
+	b->efunc = efunc;
 	b->func_data = func_data;
 }
 
@@ -49,7 +65,7 @@ void block_add (block* b, size_t size, const uint8_t data[]) {
 		size_t trailing_size = (size_mask + 1) - block_size;
 		memcpy (&buffer[block_size], data, trailing_size);
 		b->full_size += trailing_size;
-		b->func (b, buffer, 1, true);
+		b->pfunc (b, buffer, 1, true);
 		data += trailing_size;
 		size -= trailing_size;
 	}
@@ -58,7 +74,7 @@ void block_add (block* b, size_t size, const uint8_t data[]) {
 	size_t size_full_blocks = size - (size & size_mask);
 	b->full_size += size_full_blocks;
 	size_t times = size >> shift_from_size (b->max_size);
-	b->func (b, data, times, true);
+	b->pfunc (b, data, times, true);
 	data += size_full_blocks;
 	
 	/* copy the remaining data */
@@ -77,23 +93,25 @@ void block_util_finalize (block* b, block_flag flags) {
 	uint_fast8_t block_size = b->full_size & size_mask;
 	bool data_bytes = true;	
 	uint_fast8_t length_index = max_block_size - length_from_flags (flags);
+	uint64_t length_field = (flags & BLOCK_COUNT) != 0 ?
+		(b->full_size >> shift_from_size (b->max_size)) + 1 : b->full_size * 8;
 	
 	if ((flags & BLOCK_EXTENDED_PADDING) != 0 && block_size == length_index) {
 		buffer[block_size] = 0x81;
 		block_size++;
 		
 		if ((flags & BLOCK_LITTLE_ENDIAN) != 0) {
-			u64_to_u8_le (b->full_size * 8, &buffer[block_size]);
+			u64_to_u8_le (length_field, &buffer[block_size]);
 			block_size += 8;
 			memset (&buffer[block_size], 0, max_block_size - block_size);
 		} else {
 			uint_fast8_t trailing_size = max_block_size - 8 - length_index;
 			memset (&buffer[block_size], 0, trailing_size);
 			block_size += trailing_size;
-			u64_to_u8_be (b->full_size * 8, &buffer[block_size]);
+			u64_to_u8_be (length_field, &buffer[block_size]);
 		}
 		
-		b->func (b, buffer, 1, true);
+		b->pfunc (b, buffer, 1, true);
 		return;
 	}
 	
@@ -105,9 +123,11 @@ void block_util_finalize (block* b, block_flag flags) {
 	
 	if (block_size > length_index) {
 		memset (&buffer[block_size], 0, max_block_size - block_size);
-		b->func (b, buffer, 1, true);
+		b->pfunc (b, buffer, 1, true);
 		data_bytes = false;
 		block_size = 0;
+		if ((flags & BLOCK_COUNT) != 0)
+			length_field++;
 	}
 	
 	if ((flags & BLOCK_EXTENDED_PADDING) != 0) {
@@ -120,16 +140,20 @@ void block_util_finalize (block* b, block_flag flags) {
 	block_size = length_index;
 	
 	if ((flags & BLOCK_LITTLE_ENDIAN) != 0) {
-		u64_to_u8_le (b->full_size * 8, &buffer[block_size]);
+		if ((flags & BLOCK_COUNT) != 0)
+		u64_to_u8_le (length_field, &buffer[block_size]);
 		block_size += 8;
 		memset (&buffer[block_size], 0, max_block_size - block_size);
 	} else {
 		uint_fast8_t trailing_size = max_block_size - 8 - length_index;
 		memset (&buffer[block_size], 0, trailing_size);
 		block_size += trailing_size;
-		u64_to_u8_be (b->full_size * 8, &buffer[block_size]);
+		u64_to_u8_be (length_field, &buffer[block_size]);
 	}
-	b->func (b, buffer, 1, data_bytes);
+	b->pfunc (b, buffer, 1, data_bytes);
+	
+	if (b->efunc != NULL)
+		b->efunc (b);
 }
 
 static uint_fast8_t mask_from_size (block_size size) {
--- src/block.h
+++ src/block.h
@@ -25,23 +25,28 @@ typedef enum {
 	BLOCK_LITTLE_ENDIAN = 0x08,
 	BLOCK_BIG_ENDIAN = 0x00,
 	
-	BLOCK_SIMPLE_PADDING = 0x00,
-	BLOCK_EXTENDED_PADDING = 0x10 /* adds 1 at the start and at the end of padding (blake256)*/
+	BLOCK_SIMPLE_PADDING = 0x00, /* only a 1 at the start of padding */
+	BLOCK_EXTENDED_PADDING = 0x10, /* adds 1 at the start and at the end of padding (blake256)*/
+	
+	BLOCK_COUNT = 0x20 /* not the block length but the block count in the length field */
 } block_flag;
 
 struct block; 
 
 typedef void (*process_func)(struct block* b, const uint8_t buffer[], unsigned int n, bool data_bits);
+typedef void (*end_func)(struct block* b); /* called at the end of finalize */
 
 typedef struct block {
 	uint8_t* buffer;
 	block_size max_size;
 	uint64_t full_size;
-	process_func func;
+	process_func pfunc;
+	end_func efunc;
 	void* func_data;
 } block;
 
 void block_init (block* b, block_size max_size, uint8_t* buffer, process_func func, void* func_data);
+void block_init_with_end (block* b, block_size max_size, uint8_t* buffer, process_func pfunc, end_func efunc, void* func_data);
 void block_add (block* b, size_t size, const uint8_t data[]);
 
 void block_util_finalize (block* b, block_flag flags);
