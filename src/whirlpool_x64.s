a689d3a01ab7671209f12a67e26d43a3a621b452
Fabjan Sukalia <fsukalia@gmail.com>
2015-08-12 21:35:49 +0200

    whirlpool_x64: performance tweaks; now faster than the c version

--- src/whirlpool_x64.s
+++ src/whirlpool_x64.s
@@ -43,12 +43,12 @@ whirlpool_process_blocks_asm: /* (rdi: uint8_t block[], rsi: uint8_t hash[8][8],
 	mov rcx, 10
 .Lloop:
 	/*
-	 * rax:
+	 * rax: t2
 	 * rbx: t1
 	 * rcx: loop counter 
 	 * rdx: block counter
 	 * rsp: stack pointer
-	 * rbp:
+	 * rbp: t3
 	 * rsi: hash
 	 * rdi: block
 	 * r8 : tmp[0]
@@ -60,24 +60,38 @@ whirlpool_process_blocks_asm: /* (rdi: uint8_t block[], rsi: uint8_t hash[8][8],
 	 * r14: tmp[6]
 	 * r15: tmp[7]
 	 */
-.macro table ti, reg
-	mov rbx, rax
-.if \ti == 7
-	and rbx, 0xFF
-	mov \reg, whirlpool_table\ti[8 * rbx]
-.else
-	shr rbx, (56 - (\ti * 8) - 3)
-	and rbx, 0x07F8
-	mov \reg, whirlpool_table\ti[rbx]
+.macro table ti1, reg1, ti2, reg2
+	movzx ebx, ah
+	mov \reg1, whirlpool_table\ti1[8 * ebx]
+	movzx ebp, al
+	mov \reg2, whirlpool_table\ti2[8 * ebp]
+.if \ti1 != 0
+	shr rax, 16
+.endif
+.endm
+
+.macro tableX2 ti1, reg1, ti2, reg2
+	movzx ebx, ah
+	movzx ebp, al
+	mov rbx, whirlpool_table\ti1[8 * ebx]
+	mov rbp, whirlpool_table\ti2[8 * ebp]
+	xor \reg1, rbx
+	xor \reg2, rbp
+
+.if \ti1 != 0
+	shr rax, 16
 .endif
 .endm
 
 .macro tableX ti, reg
-	mov rbx, rax
 .if \ti == 7
-	and rbx, 0xFF
-	mov rbx, whirlpool_table\ti[8 * rbx]
+	movzx ebx, al
+	mov rbx, whirlpool_table\ti[8 * ebx]
+.elseif \ti == 6
+	movzx ebx, ah
+	mov rbx, whirlpool_table\ti[8 * ebx]
 .else
+	mov rbx, rax
 	shr rbx, (56 - (\ti * 8) - 3)
 	and rbx, 0x07F8
 	mov rbx, whirlpool_table\ti[rbx]
@@ -86,84 +100,52 @@ whirlpool_process_blocks_asm: /* (rdi: uint8_t block[], rsi: uint8_t hash[8][8],
 .endm
 
 	mov rax, [rsp + 0] /* key[0] */
-	table 0, r8
-	table 1, r9
-	table 2, r10
-	table 3, r11
-	table 4, r12
-	table 5, r13
-	table 6, r14
-	table 7, r15
+	table 6, r14, 7, r15
+	table 4, r12, 5, r13
+	table 2, r10, 3, r11
+	table 0, r8,  1, r9
 
 	mov rax, [rsp + 8] /* key[1] */
-	tableX 7, r8
-	tableX 0, r9
-	tableX 1, r10
-	tableX 2, r11
-	tableX 3, r12
-	tableX 4, r13
-	tableX 5, r14
-	tableX 6, r15
+	tableX2 6, r15, 7, r8
+	tableX2 4, r13, 5, r14
+	tableX2 2, r11, 3, r12
+	tableX2 0, r9,  1, r10
 
 	mov rax, [rsp + 16] /* key[2] */
-	tableX 6, r8
-	tableX 7, r9
-	tableX 0, r10
-	tableX 1, r11
-	tableX 2, r12
-	tableX 3, r13
-	tableX 4, r14
-	tableX 5, r15
+	tableX2 6, r8,  7, r9
+	tableX2 4, r14, 5, r15
+	tableX2 2, r12, 3, r13
+	tableX2 0, r10, 1, r11
 
 	mov rax, [rsp + 24] /* key[3] */
-	tableX 5, r8
-	tableX 6, r9
-	tableX 7, r10
-	tableX 0, r11
-	tableX 1, r12
-	tableX 2, r13
-	tableX 3, r14
-	tableX 4, r15
+	tableX2 6, r9,  7, r10
+	tableX2 4, r15, 5, r8
+	tableX2 2, r13, 3, r14
+	tableX2 0, r11, 1, r12
 
 	mov rax, [rsp + 32] /* key[4] */
-	tableX 4, r8
-	tableX 5, r9
-	tableX 6, r10
-	tableX 7, r11
-	tableX 0, r12
-	tableX 1, r13
-	tableX 2, r14
-	tableX 3, r15
-	
+	tableX2 6, r10, 7, r11
+	tableX2 4, r8,  5, r9
+	tableX2 2, r14, 3, r15
+	tableX2 0, r12, 1, r13
+
 	mov rax, [rsp + 40] /* key[5] */
-	tableX 3, r8
-	tableX 4, r9
-	tableX 5, r10
-	tableX 6, r11
-	tableX 7, r12
-	tableX 0, r13
-	tableX 1, r14
-	tableX 2, r15
+	tableX2 6, r11, 7, r12
+	tableX2 4, r9,  5, r10
+	tableX2 2, r15, 3, r8
+	tableX2 0, r13, 1, r14
 
 	mov rax, [rsp + 48] /* key[6] */
-	tableX 2, r8
-	tableX 3, r9
-	tableX 4, r10
-	tableX 5, r11
-	tableX 6, r12
-	tableX 7, r13
-	tableX 0, r14
-	tableX 1, r15
+	tableX2 6, r12, 7, r13
+	tableX2 4, r10, 5, r11
+	tableX2 2, r8,  3, r9
+	tableX2 0, r14, 1, r15
 	
 	mov rax, [rsp + 56] /* key[7] */
-	tableX 1, r8
-	tableX 2, r9
-	tableX 3, r10
-	tableX 4, r11
-	tableX 5, r12
-	tableX 6, r13
-	tableX 7, r14
-	tableX 0, r15
+	tableX2 6, r13, 7, r14
+	tableX2 4, r11, 5, r12
+	tableX2 2, r9,  3, r10
+	tableX2 0, r15, 1, r8
 
 	/* round constant */
 	mov rbx, 10
@@ -181,92 +163,62 @@ whirlpool_process_blocks_asm: /* (rdi: uint8_t block[], rsi: uint8_t hash[8][8],
 	mov [rsp + 56], r15
 
 	mov rax, [rsp + 64] /* state[0] */
-	tableX 0, r8
-	tableX 1, r9
-	tableX 2, r10
-	tableX 3, r11
-	tableX 4, r12
-	tableX 5, r13
-	tableX 6, r14
-	tableX 7, r15
+	tableX2 6, r14, 7, r15
+	tableX2 4, r12, 5, r13
+	tableX2 2, r10, 3, r11
+	tableX2 0, r8,  1, r9
 
 	mov rax, [rsp + 72] /* state[1] */
-	tableX 7, r8
-	tableX 0, r9
-	tableX 1, r10
-	tableX 2, r11
-	tableX 3, r12
-	tableX 4, r13
-	tableX 5, r14
-	tableX 6, r15
+	tableX2 6, r15, 7, r8
+	tableX2 4, r13, 5, r14
+	tableX2 2, r11, 3, r12
+	tableX2 0, r9,  1, r10
 
 	mov rax, [rsp + 80] /* state[2] */
-	tableX 6, r8
-	tableX 7, r9
-	tableX 0, r10
-	tableX 1, r11
-	tableX 2, r12
-	tableX 3, r13
-	tableX 4, r14
-	tableX 5, r15
+	tableX2 6, r8,  7, r9
+	tableX2 4, r14, 5, r15
+	tableX2 2, r12, 3, r13
+	tableX2 0, r10, 1, r11
 
 	mov rax, [rsp + 88] /* state[3] */
-	tableX 5, r8
-	tableX 6, r9
-	tableX 7, r10
-	tableX 0, r11
-	tableX 1, r12
-	tableX 2, r13
-	tableX 3, r14
-	tableX 4, r15
+	tableX2 6, r9,  7, r10
+	tableX2 4, r15, 5, r8
+	tableX2 2, r13, 3, r14
+	tableX2 0, r11, 1, r12
 
 	mov rax, [rsp + 96] /* state[4] */
-	tableX 4, r8
-	tableX 5, r9
-	tableX 6, r10
-	tableX 7, r11
-	tableX 0, r12
-	tableX 1, r13
-	tableX 2, r14
-	tableX 3, r15
+	tableX2 6, r10, 7, r11
+	tableX2 4, r8,  5, r9
+	tableX2 2, r14, 3, r15
+	tableX2 0, r12, 1, r13
 	
 	mov rax, [rsp + 104] /* state[5] */
-	tableX 3, r8
-	tableX 4, r9
-	tableX 5, r10
-	tableX 6, r11
-	tableX 7, r12
-	tableX 0, r13
-	tableX 1, r14
-	tableX 2, r15
+	tableX2 6, r11, 7, r12
+	tableX2 4, r9,  5, r10
+	tableX2 2, r15, 3, r8
+	tableX2 0, r13, 1, r14
 
 	mov rax, [rsp + 112] /* state[6] */
-	tableX 2, r8
-	tableX 3, r9
-	tableX 4, r10
-	tableX 5, r11
-	tableX 6, r12
-	tableX 7, r13
-	tableX 0, r14
-	tableX 1, r15
+	tableX2 6, r12, 7, r13
+	tableX2 4, r10, 5, r11
+	tableX2 2, r8,  3, r9
+	tableX2 0, r14, 1, r15
 	
 	mov rax, [rsp + 120] /* state[7] */
-	tableX 1, r8
-	tableX 2, r9
-	tableX 3, r10
-	tableX 4, r11
-	tableX 5, r12
-	tableX 6, r13
-	tableX 7, r14
-	tableX 0, r15
-
-	mov [rsp +  64], r8
-	mov [rsp +  72], r9
-	mov [rsp +  80], r10
-	mov [rsp +  88], r11
-	mov [rsp +  96], r12
+	tableX2 6, r13, 7, r14
 	mov [rsp + 104], r13
 	mov [rsp + 112], r14
+
+	tableX2 4, r11, 5, r12
+	mov [rsp +  88], r11
+	mov [rsp +  96], r12
+
+	tableX2 2, r9,  3, r10
+	mov [rsp +  72], r9
+	mov [rsp +  80], r10
+
+	tableX2 0, r15, 1, r8
+	mov [rsp +  64], r8
 	mov [rsp + 120], r15
 
 	dec rcx
@@ -276,10 +228,10 @@ whirlpool_process_blocks_asm: /* (rdi: uint8_t block[], rsi: uint8_t hash[8][8],
 	/* hash[i] ^= state[x] ^ rev(block[i]); */
 .macro wb reg, offset
 	mov rbx, [rdi + \offset]
+	mov rbp, [rsi + \offset]
 	bswap rbx
 	xor \reg, rbx
-	mov rbx, [rsi + \offset]
-	xor \reg, rbx
+	xor \reg, rbp
 	mov [rsi + \offset], \reg
 .endm
 
